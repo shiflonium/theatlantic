77e522fb9b9fa34b21e342681a2690e4ee0516b4
shifloni <yonatanshimroni@gmail.com>
2019-04-15 16:24:16 -0400

    saving of all data works including detection of changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import models
 from werkzeug.utils import secure_filename
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,44 +23,75 @@ def index():
         content = [line.strip().split('\t') for line in content]
 
         for line in content:
+            # map columns to names
             customer_id = int(line[0])
+            first_name = line[1]
+            last_name = line[2]
+            address = line[3]
+            state = line[4]
+            zip_code = line[5]
+            status = line[6]
+            product_id = int(line[7])
+            product_name = line[8]
+            price = line[9]
+            date = datetime.strptime(line[10], '%Y-%m-%dT%H:%MZ')
             #check if customer in database
             customer = db.session.query(models.Customer).filter(models.Customer.id == customer_id).first()
             if not customer:
                 c = models.Customer(
-                    line[0],
-                    line[1],
-                    line[2],
-                    line[3],
-                    line[4],
-                    line[5]
+                    customer_id,
+                    first_name,
+                    last_name,
+                    address,
+                    state,
+                    zip_code
                 )
                 db.session.add(c)
             else:
-                if customer.address != line[3] or customer.state != line[4] or customer.zip != line[5]:
-                    print(customer.address, line[3])
-                    customer.address = line[3]
-                    customer.state = line[4]
-                    customer.zip = line[5]
+                if customer.address != line[3] or customer.address != state or customer.zip != zip_code:
+
+                    customer.address = address
+                    customer.state = state
+                    customer.zip = zip_code
+
                     db.session.commit()
 
-            product_id = int(line[7])
+
             # check if product in db
             product = db.session.query(models.Product).filter(models.Product.id == product_id).first()
             if not product:
                 p = models.Product(
-                    line[7],
-                    line[8],
-                    line[9],
+                    product_id,
+                    product_name
                 )
                 db.session.add(p)
 
+            # process order data
+            if status == 'canceled':
+                o = db.session.query(models.Order).filter(
+                    models.Order.customer_id == customer_id,
+                    models.Order.product_id == product_id
+                ).first()
+
+                if o:
+                    if o.status == 'new' and o.date < date:
+                        o.status = status
+                        db.session.commit()
+
+
+            else:
+                new_order = models.Order(
+                    customer_id,
+                    product_id,
+                    price,
+                    status,
+                    date
+                )
+                db.session.add(new_order)
+
         db.session.commit()
 
-
-
-
-    return render_template('templates.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
